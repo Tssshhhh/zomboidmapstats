a0830c34d5474f0a62b5ffbec88248f4c6bdb3a1
tssshhhh <k@exmpp.org>
2023-10-20 15:50:48 +0300

    deleted break in loop. Added dict return of coord-items

--- hexparser.py
+++ hexparser.py
@@ -12,7 +12,7 @@ def parse_map(save_path: str, item_names: list) -> dict:
     map_items = {}
     files = [f for f in os.listdir(save_path) if os.path.isfile(save_path + f)]
     for file in files:
-        if not file.startswith('map_'):
+        if not (file.startswith('map_') and file[-5].isdigit()):
             continue
         coords = tuple(int(x) for x in file.split('map_')[1].split('.')[0].split('_'))
         with open(save_path + file, 'rb') as bin_file:
@@ -26,14 +26,11 @@ def parse_map(save_path: str, item_names: list) -> dict:
         map_items[coords] = {}
         for item in item_names:
             if item in letters:
-                if item not in map_items[coords]:
-                    map_items[coords][item] = 0
-                map_items[coords][item] += 1
-        break
-
+                map_items[coords][item] = letters.count(item)
+    return map_items
 
 
 if __name__ == '__main__':
     save_path = 'save\\'
     item_names = get_item_names(items_list)
-    parse_map(save_path, item_names)
+    map_items = parse_map(save_path, item_names)
